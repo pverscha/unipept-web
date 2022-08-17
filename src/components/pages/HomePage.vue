bb6adc321b67c1ab96061787709c5b85ea1ed89c
tibvdm <tibovandemoortele@hotmail.com>
2022-08-18 00:52:02 +0200

    seperate card component using a slot for the html body

--- src/components/pages/HomePage.vue
+++ src/components/pages/HomePage.vue
@@ -63,21 +63,96 @@
                 </v-col>
             </v-row>
             <v-row>
-                <v-col v-for="(feature, index) in features" :key="index" class="col-lg-4 col-md-6">
-                    <router-link :to="feature.link">
+                <!-- Tryptic Peptide Analysis -->
+                <v-col class="col-lg-4 col-md-6">
+                    <router-link to="/tpa">
                         <v-hover>
                             <template v-slot:default="{ hover }">
-                                <v-card :elevation="hover ? 6 : 2" class="equal">
-                                    <v-img
-                                        height="300"
-                                        :src="feature.asset"
-                                        :alt="feature.asset"
-                                    />
+                                <HomePageCard :hover="hover" title="Tryptic Peptide Analysis" :asset="require('@/assets/home-feature-single.png')">
+                                    With tryptic peptide analysis, you can submit a single <span class='font-weight-bold'>tryptic peptide</span> that can be 5 
+                                    to 50 residues long. The application will respond with a list of <span class='font-weight-bold'>all Uniprot entries</span> 
+                                    wherein the peptide was found along with a complete taxonomic lineage derived from the NCBI taxonomy. These lineages are 
+                                    presented as a comprehensible table and using an interactive tree view.
+                                </HomePageCard>
+                            </template>
+                        </v-hover>
+                    </router-link>
+                </v-col>
 
-                                    <v-card-title class="feature-title">{{ feature.name }}</v-card-title>
+                <!-- Metaproteomics Analysis -->
+                <v-col class="col-lg-4 col-md-6">
+                    <router-link to="/mpa">
+                        <v-hover>
+                            <template v-slot:default="{ hover }">
+                                <HomePageCard :hover="hover" title="Metaproteomics Analysis" :asset="require('@/assets/home-feature-multi.svg')">
+                                    Metaproteomics analysis helps you analyze <span class='font-weight-bold'>lists of tryptic peptides</span>, e.g. 
+                                    extracted from an environmental sample using shotgun tandem mass spectrometric methods. Of these peptides, the 
+                                    lowest common ancestors (LCA) will be calculated. These LCAs will be bundled and displayed in an interactive treemap 
+                                    giving you insight into the <span class='font-weight-bold'>biodiversity of your sample</span>.
+                                </HomePageCard>
+                            </template>
+                        </v-hover>
+                    </router-link>
+                </v-col>
 
-                                    <v-card-text class="mt-4" v-html="feature.description" />
-                                </v-card>
+                <!-- Unipept Desktop -->
+                <v-col class="col-lg-4 col-md-6">
+                    <router-link to="/desktop">
+                        <v-hover>
+                            <template v-slot:default="{ hover }">
+                                <HomePageCard :hover="hover" title="Unipept Desktop" :asset="require('@/assets/home-feature-desktop.png')">
+                                    The Unipept Desktop application allows to analyse larger samples, to store analysis results offline, to extensively 
+                                    compare assays with each other and to better manage samples. This application is powered by the Electron framework and 
+                                    runs on almost all systems. This allows you to improve the understanding of all interactions that are taking place in a 
+                                    complex environment.
+                                </HomePageCard>
+                            </template>
+                        </v-hover>
+                    </router-link>
+                </v-col>
+
+                <!-- API Documentation -->
+                <v-col class="col-lg-4 col-md-6">
+                    <router-link to="/apidocs">
+                        <v-hover>
+                            <template v-slot:default="{ hover }">
+                                <HomePageCard :hover="hover" title="API Documentation" :asset="require('@/assets/home-feature-api.svg')">
+                                    Unipept offers most of its peptide analysis features as a <span class='font-weight-bold'>web service</span>. This enables 
+                                    the integration of Unipept functionality into other applications and the creation of batch processing scripts. These 
+                                    <span class='font-weight-bold'>documentation pages</span> describe the available features of the API, how to access them and 
+                                    plenty examples.
+                                </HomePageCard>
+                            </template>
+                        </v-hover>
+                    </router-link>
+                </v-col>
+
+                <!-- CLI Documentation -->
+                <v-col class="col-lg-4 col-md-6">
+                    <router-link to="/clidocs">
+                        <v-hover>
+                            <template v-slot:default="{ hover }">
+                                <HomePageCard :hover="hover" title="CLI Documentation" :asset="require('@/assets/home-feature-cli.svg')">
+                                    The Unipept <span class='font-weight-bold'>command line interface</span> (CLI) is a wrapper around the Unipept API that 
+                                    offers an easy way to <span class='font-weight-bold'>integrate Unipept</span> functionality into your data processing 
+                                    <span class='font-weight-bold'>pipelines and scripts</span>. These pages cover installation and usage instructions, an 
+                                    overview of all available options and several use cases.
+                                </HomePageCard>
+                            </template>
+                        </v-hover>
+                    </router-link>
+                </v-col>
+
+                <!-- Metagenomics Analysis -->
+                <v-col class="col-lg-4 col-md-6">
+                    <router-link to="/umgap">
+                        <v-hover>
+                            <template v-slot:default="{ hover }">
+                                <HomePageCard :hover="hover" title="UMGAP" :asset="require('@/assets/home-feature-umgap.svg')">
+                                    Use the Unipept MetaGenomics Analysis Pipeline to assign taxonomic labels to your <span class='font-weight-bold'>shotgun 
+                                    metagenomics reads</span>. The results are available as taxonomic frequency tables and interactive visualizations. UMGAP 
+                                    is a collection of CLI tools that can be combined to identify shotgun metagenomics reads.
+                                </HomePageCard>
                             </template>
                         </v-hover>
                     </router-link>
@@ -89,53 +164,12 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import HomePageCard from "../cards/HomePageCard.vue";
 
 export default defineComponent({
     name: "HomePage",
     setup() { },
-    components: { },
-    data() {
-        return {
-            features: [
-                { 
-                    name: "Tryptic Peptide Analysis", 
-                    description: "With tryptic peptide analysis, you can submit a single <span class='font-weight-bold'>tryptic peptide</span> that can be 5 to 50 residues long. The application will respond with a list of <span class='font-weight-bold'>all Uniprot entries</span> wherein the peptide was found along with a complete taxonomic lineage derived from the NCBI taxonomy. These lineages are presented as a comprehensible table and using an interactive tree view.", 
-                    asset: require("@/assets/home-feature-single.png"),
-                    link: "/tpa"
-                },
-                { 
-                    name: "Metaproteome Analysis", 
-                    description: "Metaproteomics analysis helps you analyze <span class='font-weight-bold'>lists of tryptic peptides</span>, e.g. extracted from an environmental sample using shotgun tandem mass spectrometric methods. Of these peptides, the lowest common ancestors (LCA) will be calculated. These LCAs will be bundled and displayed in an interactive treemap giving you insight into the <span class='font-weight-bold'>biodiversity of your sample</span>.", 
-                    asset: require("@/assets/home-feature-multi.svg"),
-                    link: "/mpa"
-                },
-                { 
-                    name: "Unipept Desktop", 
-                    description: "The Unipept Desktop application allows to analyse larger samples, to store analysis results offline, to extensively compare assays with each other and to better manage samples. This application is powered by the Electron framework and runs on almost all systems. This allows you to improve the understanding of all interactions that are taking place in a complex environment.", 
-                    asset: require("@/assets/home-feature-desktop.png"),
-                    link: "/desktop"
-                },
-                { 
-                    name: "API Documentation", 
-                    description: "Unipept offers most of its peptide analysis features as a <span class='font-weight-bold'>web service</span>. This enables the integration of Unipept functionality into other applications and the creation of batch processing scripts. These <span class='font-weight-bold'>documentation pages</span> describe the available features of the API, how to access them and plenty examples.", 
-                    asset: require("@/assets/home-feature-api.svg"),
-                    link: "/apidocs"
-                },
-                {
-                    name: "CLI Documentation", 
-                    description: "The Unipept <span class='font-weight-bold'>command line interface</span> (CLI) is a wrapper around the Unipept API that offers an easy way to <span class='font-weight-bold'>integrate Unipept</span> functionality into your data processing <span class='font-weight-bold'>pipelines and scripts</span>. These pages cover installation and usage instructions, an overview of all available options and several use cases.", 
-                    asset: require("@/assets/home-feature-cli.svg"),
-                    link: "/clidocs"
-                },
-                { 
-                    name: "UMGAP", 
-                    description: "Use the Unipept MetaGenomics Analysis Pipeline to assign taxonomic labels to your <span class='font-weight-bold'>shotgun metagenomics reads</span>. The results are available as taxonomic frequency tables and interactive visualizations. UMGAP is a collection of CLI tools that can be combined to identify shotgun metagenomics reads.", 
-                    asset: require("@/assets/home-feature-umgap.svg"),
-                    link: "/umgap"
-                }
-            ]
-        }
-    }
+    components: { HomePageCard },
 });
 </script>
 
@@ -146,16 +180,5 @@ export default defineComponent({
 
     a {
         text-decoration: none;
-    }
-
-    .feature-title {
-        opacity: 0.70;
-        background-color: black;
-        color: white;
-        margin-top: -64px;
-    }
-
-    .equal {
-        height: 100%;
     }
 </style>
