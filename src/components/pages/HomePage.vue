26f2e906a3354252c54fc812768889a12aa634d5
tibvdm <tibovandemoortele@hotmail.com>
2022-08-17 10:50:09 +0200

    routing for feature cards

--- src/components/pages/HomePage.vue
+++ src/components/pages/HomePage.vue
@@ -64,21 +64,23 @@
             </v-row>
             <v-row>
                 <v-col v-for="(feature, index) in features" :key="index" class="col-lg-4 col-md-6">
-                    <v-hover>
-                        <template v-slot:default="{ hover }">
-                            <v-card :elevation="hover ? 6 : 2">
-                                <v-img
-                                    height="250"
-                                    :src="feature.asset"
-                                    :alt="feature.asset"
-                                />
+                    <router-link :to="feature.link">
+                        <v-hover>
+                            <template v-slot:default="{ hover }">
+                                <v-card :elevation="hover ? 6 : 2">
+                                    <v-img
+                                        height="250"
+                                        :src="feature.asset"
+                                        :alt="feature.asset"
+                                    />
 
-                                <v-card-title class="m-n16">{{ feature.name }}</v-card-title>
+                                    <v-card-title class="m-n16">{{ feature.name }}</v-card-title>
 
-                                <v-card-text v-html="feature.description" />
-                            </v-card>
-                        </template>
-                    </v-hover>
+                                    <v-card-text v-html="feature.description" />
+                                </v-card>
+                            </template>
+                        </v-hover>
+                    </router-link>
                 </v-col>
             </v-row>
         </v-container>
@@ -98,32 +100,38 @@ export default defineComponent({
                 { 
                     name: "Tryptic Peptide Analysis", 
                     description: "With tryptic peptide analysis, you can submit a single <span class='font-weight-bold'>tryptic peptide</span> that can be 5 to 50 residues long. The application will respond with a list of <span class='font-weight-bold'>all Uniprot entries</span> wherein the peptide was found along with a complete taxonomic lineage derived from the NCBI taxonomy. These lineages are presented as a comprehensible table and using an interactive tree view.", 
-                    asset: require("@/assets/home-feature-single.png") 
+                    asset: require("@/assets/home-feature-single.png"),
+                    link: "/tpa"
                 },
                 { 
                     name: "Metaproteome Analysis", 
                     description: "Metaproteomics analysis helps you analyze <span class='font-weight-bold'>lists of tryptic peptides</span>, e.g. extracted from an environmental sample using shotgun tandem mass spectrometric methods. Of these peptides, the lowest common ancestors (LCA) will be calculated. These LCAs will be bundled and displayed in an interactive treemap giving you insight into the <span class='font-weight-bold'>biodiversity of your sample</span>.", 
-                    asset: require("@/assets/home-feature-multi.svg") 
+                    asset: require("@/assets/home-feature-multi.svg"),
+                    link: "/mpa"
                 },
                 { 
                     name: "Unipept Desktop", 
                     description: "The Unipept Desktop application allows to analyse larger samples, to store analysis results offline, to extensively compare assays with each other and to better manage samples. This application is powered by the Electron framework and runs on almost all systems. This allows you to improve the understanding of all interactions that are taking place in a complex environment.", 
-                    asset: require("@/assets/home-feature-desktop.png") 
+                    asset: require("@/assets/home-feature-desktop.png"),
+                    link: "/desktop"
                 },
                 { 
                     name: "API Documentation", 
                     description: "Unipept offers most of its peptide analysis features as a <span class='font-weight-bold'>web service</span>. This enables the integration of Unipept functionality into other applications and the creation of batch processing scripts. These <span class='font-weight-bold'>documentation pages</span> describe the available features of the API, how to access them and plenty examples.", 
-                    asset: require("@/assets/image-placeholder.jpg") 
+                    asset: require("@/assets/image-placeholder.jpg"),
+                    link: "/apidocs"
                 },
                 { 
                     name: "CLI Documentation", 
                     description: "The Unipept <span class='font-weight-bold'>command line interface</span> (CLI) is a wrapper around the Unipept API that offers an easy way to <span class='font-weight-bold'>integrate Unipept</span> functionality into your data processing <span class='font-weight-bold'>pipelines and scripts</span>. These pages cover installation and usage instructions, an overview of all available options and several use cases.", 
-                    asset: require("@/assets/image-placeholder.jpg") 
+                    asset: require("@/assets/image-placeholder.jpg"),
+                    link: "/clidocs"
                 },
                 { 
                     name: "UMGAP", 
                     description: "Use the Unipept MetaGenomics Analysis Pipeline to assign taxonomic labels to your <span class='font-weight-bold'>shotgun metagenomics reads</span>. The results are available as taxonomic frequency tables and interactive visualizations. UMGAP is a collection of CLI tools that can be combined to identify shotgun metagenomics reads.", 
-                    asset: require("@/assets/home-feature-umgap.svg") 
+                    asset: require("@/assets/home-feature-umgap.svg"),
+                    link: "/umgap"
                 }
             ]
         }
--- src/plugins/router.ts
+++ src/plugins/router.ts
@@ -2,6 +2,12 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import HomePage from "@/components/pages/HomePage.vue";
 import NewsPage from "@/components/pages/NewsPage.vue";
+import TrypticPeptideAnalysisPage from "@/components/pages/features/TrypticPeptideAnalysisPage.vue";
+import MetaproteomeAnalysisPage from "@/components/pages/features/MetaproteomeAnalysisPage.vue";
+import APIPage from "@/components/pages/features/APIPage.vue";
+import CLIPage from "@/components/pages/features/CLIPage.vue";
+import MetagenomicsPage from "@/components/pages/features/MetagenomicsPage.vue";
+import DesktopPage from "@/components/pages/features/DesktopPage.vue";
 
 Vue.use(VueRouter);
 
@@ -11,9 +17,33 @@ const routes = [
         component: HomePage
     },
     {
+        path: "/tpa",
+        component: TrypticPeptideAnalysisPage
+    },
+    {
+        path: "/mpa",
+        component: MetaproteomeAnalysisPage
+    },
+    {
+        path: "/apidocs",
+        component: APIPage
+    },
+    {
+        path: "/clidocs",
+        component: CLIPage
+    },
+    {
+        path: "/umgap",
+        component: MetagenomicsPage
+    },
+    {
+        path: "/desktop",
+        component: DesktopPage
+    },
+    {
         path: "/posts",
         component: NewsPage
-    }
+    },
 ];
 
 export default new VueRouter({
