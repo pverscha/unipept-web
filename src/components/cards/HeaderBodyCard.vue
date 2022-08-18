0ec87ecae136ad039b0caa005f69c7eac1aafbde
tibvdm <tibovandemoortele@hotmail.com>
2022-08-18 22:32:40 +0200

    request/response/parameter cards

---
+++ src/components/cards/HeaderBodyCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-card>
+        <v-card-title class="blue white--text">{{ title }}</v-card-title>
+
+        <v-card-text class="mt-4">
+            <slot></slot>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script setup lang="ts">
+export interface Props {
+    title: String
+}
+
+/* eslint-disable */
+defineProps<Props>()
+</script>
+
+<style scoped>
+</style>
