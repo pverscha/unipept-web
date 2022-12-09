a8e71938e282733392e84dd416a386cbf206fa8b
tibvdm <tibovandemoortele@hotmail.com>
2022-12-09 14:49:42 +0100

    compare multiple samples

--- src/components/cards/analysis/multi/SwitchDatasetCard.vue
+++ src/components/cards/analysis/multi/SwitchDatasetCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-card class="d-flex flex-column flex-grow-1">
+    <v-card class="d-flex flex-column">
         <v-tabs
             class="flex-grow-0"
             style="pointer-events: none;"
@@ -14,7 +14,7 @@
             <span>Please add one or more datasets by clicking the plus button above...</span>
         </v-card-text>
 
-        <div v-else class="growing-list">
+        <div v-else class="flex-grow-1">
             <v-list two-line v-model="multiAnalysisStore.activeAssayStatus">
                 <v-list-item
                     v-for="assayStatus in assayStatuses"
@@ -63,14 +63,48 @@
                 </v-list-item>
             </v-list>
         </div>
+        <v-card-text>
+            <v-divider></v-divider>
+            <div class="text-center pt-4">
+                <tooltip message="Compare samples above using a heatmap.">
+                    <v-btn :disabled="false" @click="dialogOpen = true">
+                        Compare samples
+                    </v-btn>
+                </tooltip>
+            </div>
+        </v-card-text>
+
+        <v-dialog v-model="dialogOpen" width="1000px">
+            <v-card style="min-height: 700px;">
+                <v-card-title color="primary" >
+                    Heatmap wizard
+                    <v-spacer></v-spacer>
+                    <v-btn icon @click="dialogOpen = false">
+                        <v-icon>mdi-close</v-icon>
+                    </v-btn>
+                </v-card-title>
+                <div>
+                    <HeatmapWizardMulti
+                        v-if="multiAnalysisStore.activeAssayStatus"
+                        :loading="!multiAnalysisStore.analysisCompleted(multiAnalysisStore.activeAssayStatus.assay.id)"
+                        :assays="multiAnalysisStore.assayStatuses"
+                    />
+                    <div v-else style="display: flex; justify-content: center;">
+                        <div class="text-xs-center" style="margin-top: 25px;">
+                            <v-progress-circular indeterminate color="primary"></v-progress-circular>
+                        </div>
+                    </div>
+                </div>
+            </v-card>
+        </v-dialog>
     </v-card>
 </template>
 
 <script setup lang="ts">
 import useMultiAnalysis from '@/stores/MultiAnalysisStore';
 import { storeToRefs } from 'pinia';
-import { Assay, StringUtils, Tooltip } from 'unipept-web-components';
-import { withDefaults, defineProps } from 'vue';
+import { Assay, StringUtils, Tooltip, HeatmapWizardMulti } from 'unipept-web-components';
+import { withDefaults, defineProps, ref } from 'vue';
 
 export interface Props {
     assaySelectionInProgress: boolean
@@ -83,6 +117,8 @@ withDefaults(defineProps<Props>(), {
 const multiAnalysisStore = useMultiAnalysis();
 
 const { assayStatuses } = storeToRefs(multiAnalysisStore);
+
+const dialogOpen = ref<boolean>(false);
 
 const done = (assayStatus: any) => {
     //console.log(JSON.stringify(assayStatus));
