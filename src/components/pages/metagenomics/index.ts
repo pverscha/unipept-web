03b12137935343f54ebea4a386e0fb2511406d20
tibvdm <tibovandemoortele@hotmail.com>
2023-01-12 14:33:53 +0100

    rename on export

--- src/components/pages/metagenomics/index.ts
+++ src/components/pages/metagenomics/index.ts
@@ -1,53 +1,53 @@
-import OverviewPage from "./OverviewPage.vue";
-import CaseStudiesPage from "./CaseStudiesPage.vue";
-import PreconfiguredPage from "./casestudies/PreconfiguredPage.vue";
-import AdvancedPage from "./casestudies/AdvancedPage.vue";
-import ComparativePage from "./casestudies/ComparativePage.vue";
-import Fastq2FastaPage from "./Fastq2FastaPage.vue";
-import TranslatePage from "./TranslatePage.vue";
-import Prot2TrypPage from "./Prot2TrypPage.vue";
-import Prot2KmerPage from "./Prot2KmerPage.vue";
-import FilterPage from "./FilterPage.vue";
-import Pept2LcaPage from "./Pept2LcaPage.vue";
-import Prot2Tryp2LcaPage from "./Prot2Tryp2LcaPage.vue";
-import Prot2Kmer2LcaPage from "./Prot2Kmer2LcaPage.vue";
-import BestofPage from "./BestofPage.vue";
-import SeedExtendPage from "./SeedExtendPage.vue";
-import UniqPage from "./UniqPage.vue";
-import Taxa2AggPage from "./Taxa2AggPage.vue";
-import SnapTaxonPage from "./SnapTaxonPage.vue";
-import Taxa2FreqPage from "./Taxa2FreqPage.vue";
-import Taxa2TreePage from "./Taxa2TreePage.vue";
-import TaxonomyPage from "./TaxonomyPage.vue";
-import SplitKmersPage from "./SplitKmersPage.vue";
-import JoinKmersPage from "./JoinKmersPage.vue";
-import BuildIndexPage from "./BuildIndexPage.vue";
-import PrintIndexPage from "./PrintIndexPage.vue";
+import MetagenomicsOverviewPage from "./OverviewPage.vue";
+import MetagenomicsCaseStudiesPage from "./CaseStudiesPage.vue";
+import MetagenomicsPreconfiguredPage from "./casestudies/PreconfiguredPage.vue";
+import MetagenomicsAdvancedPage from "./casestudies/AdvancedPage.vue";
+import MetagenomicsComparativePage from "./casestudies/ComparativePage.vue";
+import MetagenomicsFastq2FastaPage from "./Fastq2FastaPage.vue";
+import MetagenomicsTranslatePage from "./TranslatePage.vue";
+import MetagenomicsProt2TrypPage from "./Prot2TrypPage.vue";
+import MetagenomicsProt2KmerPage from "./Prot2KmerPage.vue";
+import MetagenomicsFilterPage from "./FilterPage.vue";
+import MetagenomicsPept2LcaPage from "./Pept2LcaPage.vue";
+import MetagenomicsProt2Tryp2LcaPage from "./Prot2Tryp2LcaPage.vue";
+import MetagenomicsProt2Kmer2LcaPage from "./Prot2Kmer2LcaPage.vue";
+import MetagenomicsBestofPage from "./BestofPage.vue";
+import MetagenomicsSeedExtendPage from "./SeedExtendPage.vue";
+import MetagenomicsUniqPage from "./UniqPage.vue";
+import MetagenomicsTaxa2AggPage from "./Taxa2AggPage.vue";
+import MetagenomicsSnapTaxonPage from "./SnapTaxonPage.vue";
+import MetagenomicsTaxa2FreqPage from "./Taxa2FreqPage.vue";
+import MetagenomicsTaxa2TreePage from "./Taxa2TreePage.vue";
+import MetagenomicsTaxonomyPage from "./TaxonomyPage.vue";
+import MetagenomicsSplitKmersPage from "./SplitKmersPage.vue";
+import MetagenomicsJoinKmersPage from "./JoinKmersPage.vue";
+import MetagenomicsBuildIndexPage from "./BuildIndexPage.vue";
+import MetagenomicsPrintIndexPage from "./PrintIndexPage.vue";
 
 export {
-    OverviewPage,
-    CaseStudiesPage,
-    PreconfiguredPage,
-    AdvancedPage,
-    ComparativePage,
-    Fastq2FastaPage,
-    TranslatePage,
-    Prot2TrypPage,
-    Prot2KmerPage,
-    FilterPage,
-    Pept2LcaPage,
-    Prot2Tryp2LcaPage,
-    Prot2Kmer2LcaPage,
-    BestofPage,
-    SeedExtendPage,
-    UniqPage,
-    Taxa2AggPage,
-    SnapTaxonPage,
-    Taxa2FreqPage,
-    Taxa2TreePage,
-    TaxonomyPage,
-    SplitKmersPage,
-    JoinKmersPage,
-    BuildIndexPage,
-    PrintIndexPage
+    MetagenomicsOverviewPage,
+    MetagenomicsCaseStudiesPage,
+    MetagenomicsPreconfiguredPage,
+    MetagenomicsAdvancedPage,
+    MetagenomicsComparativePage,
+    MetagenomicsFastq2FastaPage,
+    MetagenomicsTranslatePage,
+    MetagenomicsProt2TrypPage,
+    MetagenomicsProt2KmerPage,
+    MetagenomicsFilterPage,
+    MetagenomicsPept2LcaPage,
+    MetagenomicsProt2Tryp2LcaPage,
+    MetagenomicsProt2Kmer2LcaPage,
+    MetagenomicsBestofPage,
+    MetagenomicsSeedExtendPage,
+    MetagenomicsUniqPage,
+    MetagenomicsTaxa2AggPage,
+    MetagenomicsSnapTaxonPage,
+    MetagenomicsTaxa2FreqPage,
+    MetagenomicsTaxa2TreePage,
+    MetagenomicsTaxonomyPage,
+    MetagenomicsSplitKmersPage,
+    MetagenomicsJoinKmersPage,
+    MetagenomicsBuildIndexPage,
+    MetagenomicsPrintIndexPage
 };
